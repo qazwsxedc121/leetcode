e1394f2036231bd35a6006aa5cee5f7c4644fda3
guoxc <qazwsxedc121@gmail.com>
2015-08-20 22:21:42 +0800

    solved ugly-number

---
+++ src/UglyNumber.java
@@ -0,0 +1,17 @@
+/**
+ * User: guoxc
+ * Date: 15-8-20
+ * Time: 22:21
+ */
+public class UglyNumber {
+
+        public boolean isUgly(int num) {
+            if(num == 0) return false;
+            if(num == 1) return true;
+            if(num % 2 == 0) return isUgly(num / 2);
+            else if(num % 3 == 0) return isUgly(num / 3);
+            else if(num % 5 == 0) return isUgly(num / 5);
+            else return false;
+        }
+
+}
